c0904945fec982cb19c3fe23826892d20f1ee198
Екатерина Бакланова <ekakotelevskaya@yandex.ru>
2022-09-18 15:44:11 +0300

    initial commit

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+    public void shouldTestPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"));
+        
+
+    }
+}
+
